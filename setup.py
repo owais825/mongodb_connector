c114e12337e08678d78655b123817aff24ba32f0
Owais Wani <owais825@gmail.com>
2025-11-06 14:33:04 +0530

    version updated

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path: str) -> List[str]:
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 REPO_NAME = "mongodb_connector"
 PKG_NAME = "databaseautomation"
 AUTHOR_USER_NAME = "owais825"
